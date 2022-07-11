486748b1bd56b3459d5526ed4478ff35f94e8815
Zlatko <iliev.zlatko@gmail.com>
2022-07-11 17:27:53 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Zlatko Iliev on 11.07.22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
